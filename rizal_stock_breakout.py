0a45ef7dff458ac23760b047ea15e4df45d4a68b
Nor Rizal <dreameshuggah@yahoo.com>
2025-01-28 11:58:25 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -74,7 +74,7 @@ ticker_list = sorted(list(set(['ADSK', 'CRM', 'MMM', 'ADBE', 'AMD', 'APD', 'ABNB
                        
 st.title('Stock Break Out')
 #ticker = st.multiselect('Select a ticker:',ticker_list,'QCOM')#,disabled=True)      
-ticker = st.selectbox('Select a ticker:',ticker_list,'QCOM')     
+ticker = st.selectbox('Select a ticker:',ticker_list)     
 
 if len(ticker)==1:    
   df = dailyClosePricesbyPeriod(ticker)
