53f447a06076501f849e10dc0bd6a83227ce6e5e
Nor Rizal <dreameshuggah@yahoo.com>
2025-01-28 18:03:56 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -74,7 +74,7 @@ for ticker_ in ticker_list:
     tmp_df = exponentialMovingAveragesClosePrice(tmp_df)
     tmp_df = findBreakOut(tmp_df,ticker_)
     if tmp_df[:1]['break_out'][0] == 'Yes':
-        breakOut_ls.append(ticker)
+        breakOut_ls.append(ticker_)
                        
                        
                        
