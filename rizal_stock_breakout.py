aea1e7412e6c209dc9e1377eabcbcb7e3c431fd9
Nor Rizal <dreameshuggah@yahoo.com>
2025-02-08 10:02:20 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -80,24 +80,76 @@ Break Out: Close Price above 10EMA, 20EMA, 50EMA
 ticker = st.multiselect('Select a ticker:',ticker_list,['QCOM'])#,disabled=True)      
 #ticker = st.selectbox('Select a ticker:',ticker_list)     
 
-if len(ticker)==1:    
-  df = dailyClosePricesbyPeriod(ticker)
-  df = exponentialMovingAveragesClosePrice(df)
-  df = findBreakOut(df,ticker)  
+if len(ticker)==1:
+    df = dailyClosePricesbyPeriod(ticker)
+    df = exponentialMovingAveragesClosePrice(df)
+    df = findBreakOut(df,ticker)  
   
-  closeTitle = ticker[0] + ' Daily Close Prices'
-  fig_close_prices = px.scatter(df, x="Date", y="Close", color="break_out"
+    closeTitle = ticker[0] + ' Daily Close Prices'
+    fig_close_prices = px.scatter(df, x="Date", y="Close", color="break_out"
                                 ,color_discrete_map = {'Yes':'green','No':'grey'}
                                 #,color_discrete_sequence = ['red','blue']
                                 , title = closeTitle )
-  st.plotly_chart(fig_close_prices, key="scatter1")#, on_select="rerun")
+    st.plotly_chart(fig_close_prices, key="scatter1")#, on_select="rerun")
   
   
-  volumeTitle = ticker[0] + ' Volume'
-  fig_volume = px.bar(df, x="Date", y="Volume" ,color="break_out"
+    volumeTitle = ticker[0] + ' Volume'
+    fig_volume = px.bar(df, x="Date", y="Volume" ,color="break_out"
                       ,color_discrete_map = {'Yes':'green','No':'grey'}
                       , title =  volumeTitle)
-  st.plotly_chart(fig_volume,key='bar1')
+    st.plotly_chart(fig_volume,key='bar1')
   
-  st.dataframe(df)
+    st.dataframe(df)
+
+    st.markdown("""Recent Financials""")
+    recent_df = fetchRecent(ticker,recent_ls)
+    qtr_df = financials_quarter(ticker)
+    
+    cols = ['date','ticker','shortName','net_interest_income_ratio','interest_income_ratio','debt_to_ebitda'
+            ,'gross_margin','npat_margin'
+            ,'Total Revenue','Net Income','Accounts Receivable','Free Cash Flow','EBITDA'
+            ,'Cash And Cash Equivalents','Capital Expenditure'
+           ]
+
+    st.dataframe(recent_df)
+    st.dataframe(qtr_df[cols],use_container_width=True)
+
+    # ======================== TAB 1 BAR CHARTS ===================  
+    st.markdown("##")
+    
+    col1_chart, col2_chart = st.columns(2)
+    
+    #col1_chart.write('\nTotal Revenue')
+    fig_revenue = px.bar(qtr_df, x="date", y="Total Revenue", color="shortName", title = 'Total Revenue' )
+    col1_chart.plotly_chart(fig_revenue, key="ticker1")#, on_select="rerun")
+    
+    #col2_chart.write('\nNet Income')
+    fig_netincome = px.bar(qtr_df, x="date", y="Net Income", color="shortName", title='Net Income')
+    col2_chart.plotly_chart(fig_netincome, key="ticker2")#, on_select="rerun")
+
+    
+    
+    col1_chart_a, col2_chart_a = st.columns(2)
+    
+    #col1_chart_a.write('\nFree Cash Flow')
+    fig_fcf = px.bar(qtr_df, x="date", y="Free Cash Flow", color="shortName", title='Free Cash Flow')
+    col1_chart_a.plotly_chart(fig_fcf, key="ticker4")#, on_select="rerun")
+
+    #col2_chart_a.write('\nAccounts Receivable')
+    fig_act = px.bar(qtr_df, x="date", y="Accounts Receivable", color="shortName", title='Accounts Receivable')
+    col2_chart_a.plotly_chart(fig_act, key="ticker5")#, on_select="rerun")
+
+
+  
+    col1_chart_b, col2_chart_b = st.columns(2)
+    
+    #col1_chart_b.write('\nCash And Cash Equivalents')
+    fig_cash = px.bar(qtr_df, x="date", y="Cash And Cash Equivalents", color="shortName", title='Cash And Cash Equivalents')
+    col1_chart_b.plotly_chart(fig_cash, key="ticker3")#, on_select="rerun")
+    
+    #col2_chart_b.write('\nCapital Expenditure')
+    fig_capex = px.bar(qtr_df, x="date", y="Capital Expenditure", color="shortName",title='Capital Expenditure')
+    col2_chart_b.plotly_chart(fig_capex, key="ticker6")#, on_select="rerun")
+
+  
 
