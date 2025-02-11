0e23bef58a118754851fd52877bca574c85c7543
Nor Rizal <dreameshuggah@yahoo.com>
2025-02-11 21:55:19 +0800

    Update rizal_3day_stock_breakout.py

--- rizal_3day_stock_breakout.py
+++ rizal_3day_stock_breakout.py
@@ -136,6 +136,7 @@ for ticker in ticker_list:
     df = dailyClosePricesbyPeriod(ticker)
     df = exponentialMovingAveragesClosePrice(df)
     df = findBreakOut(df,ticker)
+    print(df.head())
     if df['break_out'][0]=='Yes' and df['break_out'][1]=='Yes' and df['break_out'][2]=='Yes' :
         breakOut_ls.append(ticker)
 
