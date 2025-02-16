f2988fb10e3c66ec89f2c63dcd3592ab7195f946
Nor Rizal <dreameshuggah@yahoo.com>
2025-02-16 16:05:22 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -126,7 +126,7 @@ operatingMargins = round(recent_df['operatingMargins'][0],4)
 
 # ================== RED FLAGS ! ==========================
 st.markdown("####")
-st.markdown("Red Flags (if exist):")
+st.markdown("#### Red Flags (if exist):")
 if forwardPE > trailingPE:
     st.write('- forwardPE: ', forwardPE, ' > trailingPE: ', trailingPE)
 if revenueGrowth < 0 :
