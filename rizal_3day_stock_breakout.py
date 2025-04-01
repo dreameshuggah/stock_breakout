0bb17e87a819a448522785f02f8f32b235006901
Nor Rizal <dreameshuggah@yahoo.com>
2025-04-01 17:22:28 +0800

    Update rizal_3day_stock_breakout.py

--- rizal_3day_stock_breakout.py
+++ rizal_3day_stock_breakout.py
@@ -59,7 +59,7 @@ fig_scatter = px.scatter(filterDf(fin_df)
                          #, symbol = 'industry'
                          , hover_data=['ticker','shortName','revenueGrowth','forwardPE']
                          , title = 'Return On Equity vs Operating Margins: Size by Forward PE'
-                         , height = '700'
+                         #, height = '700'
                         )
 st.plotly_chart(fig_scatter, key="ticker0")#, on_select="rerun")
 
