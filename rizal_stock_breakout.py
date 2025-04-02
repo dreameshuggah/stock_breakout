0145c2e3f0278a95aa77ae1aa8de2f5b331cba63
Nor Rizal <dreameshuggah@yahoo.com>
2025-04-02 17:33:00 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -139,7 +139,7 @@ st.markdown("#####")
 #col1, col2, col3 = st.columns(3)
 st.write('__Break Out Signal (latest) :__\n *',lastBreakOutSignal)
 
-#st.markdown("#####")
+st.markdown("#####")
 st.write("__:small_red_triangle_down: Red Flags  (if exist):__")
 if forwardPE > trailingPE and trailingPE!=0:
     st.write('* __forwardPE:__ ', forwardPE, ' > trailingPE: ', trailingPE,' :x:')
@@ -149,7 +149,7 @@ if operatingMargins < 0.1 :
     st.write('* __operatingMargins:__',operatingMargins*100,'__%__',' :x:')
 
 
-#st.markdown("#####")
+st.markdown("#####")
 st.write("__:large_green_circle: Green Flags (if exist):__")
 if revenueGrowth >= 0.1 :
     st.write('* __revenueGrowth:__ ',revenueGrowth*100,'__%__',':heavy_check_mark:')
