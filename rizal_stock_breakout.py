6f1ce146b5840ef044f6a5da4ae21d0abee0d2f4
Nor Rizal <dreameshuggah@yahoo.com>
2025-04-05 21:06:02 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -150,6 +150,7 @@ with col1:
     st.write(f'__:classical_building: {shortName}__ ')
     st.write('* __Industry:__  ',industry)
     st.write('* __MarketCap:__  ',round(marketCap/1000000000,2),'__billion__' )
+    st.write('* __ForwardPE:__  ',forwardPE)
     st.write(f'* __Break Out Signal:__  {lastBreakOutSignal}',)
 
 #st.markdown("#####")
