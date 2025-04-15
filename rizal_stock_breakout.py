a1d61d851a689d9c9d131aac31de65a34bafac81
Nor Rizal <dreameshuggah@yahoo.com>
2025-04-15 21:19:43 +0800

    Update rizal_stock_breakout.py

--- rizal_stock_breakout.py
+++ rizal_stock_breakout.py
@@ -193,10 +193,10 @@ with col3:
     if operatingMargins >= 0.2 :
         st.write('* __operatingMargins:__',round(operatingMargins*100,1),'__%__',':heavy_check_mark:')
     if break_down_150ema == 'No':
-        st.write('* __Trend: Above 150 EMA!__ :heavy_check_mark:')
+        st.write('* __Trend:  Above 150 EMA!__ :heavy_check_mark:')
 
     if lastBreakOutSignal in ['Yes','Yes Buy']:
-        st.write('* __Trend: Above 50,20,10 EMA!__ :heavy_check_mark:')
+        st.write('* __Trend:  Above 50,20,10 EMA!__ :heavy_check_mark:')
     
 
 
